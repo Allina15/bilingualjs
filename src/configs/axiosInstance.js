68c2a9f6deecc8a78be9d48771aa48b7c91b0dab
Aiylchieva Rabiya <rabiyaaiylchieva@gmail.com>
2024-04-02 12:20:11 +0600

    deleted env-local

--- src/configs/axiosInstance.js
+++ src/configs/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = process.env.REACT_APP_API
+const BASE_URL = 'http://billingual.peaksoftprojects.com/'
 
 export const axiosInstance = axios.create({
    baseURL: BASE_URL,
